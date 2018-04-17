a6d617069f5b3d0b01087e8ecea565fef4317c2a
pavan kumar thikkavarapu <pavankumar.thikkavarapu@cesltd.com>
2018-04-17 17:08:58 +0530

    with routing functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { AuthService } from './auth/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
+import { HeaderComponent } from './header/header.component';
 // import { AuthService } from './auth/auth.service';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'register', component: SignupComponent },
+  { path: 'login', component: SigninComponent },
+];
+
 
 @NgModule({
   declarations: [
@@ -16,12 +28,18 @@ import { SigninComponent } from './auth/signin/signin.component';
     WarningAlertComponent,
     SuccessAlertComponent,
     SignupComponent,
-    SigninComponent
+    SigninComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    MatCardModule,
+    BrowserAnimationsModule,MatButtonModule, MatCheckboxModule,
+    RouterModule.forRoot(routes) 
   ],
+  exports: [ RouterModule ],
   providers: [AuthService],
   bootstrap: [AppComponent]
 })
